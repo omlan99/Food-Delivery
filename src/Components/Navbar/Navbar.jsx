6f3058da4c011bfb8a2fd0303aaebc3639b0ac15
Amlan <omlan10616@gmail.com>
2025-07-21 18:22:51 +0000

    added hover and active status

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.css"
 import { assets } from '../../assets/assets'
 const Navbar = () => {
+  const [menu, setMenu] = useState("home");
+
   return (
+
     <div className='navbar'>
       <img src={assets.logo} className='logo'/>
       <ul className="navbar-menu">
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Mobile App</li>
-        <li>Contact Us</li>
+        <li className={menu==="home"?"active": ""}>Home</li>
+        <li className={menu==="menu"? "active": ""}>Menu</li>
+        <li className={menu ==="mobile-app"?"active" : ""}>Mobile App</li>
+        <li className={menu === "contact-us"?"active" : ""}>Contact Us</li>
       </ul>
       <div className='navbar-right'>
         <img src={assets.search_icon} alt="" />
