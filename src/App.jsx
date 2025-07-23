9fc29d9c5bcefed5c3ad8f45ddcaea3285088fdd
Amlan <omlan10616@gmail.com>
2025-07-23 17:16:58 +0000

    added routes of nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from "./Components/Navbar/Navbar"
+import { Routes, Route } from 'react-router'
+import Home from './Pages/Cart/PlaceOrder/Home/Home'
+import Cart from './Pages/Cart/Cart'
+import PlaceOrder from './Pages/Cart/PlaceOrder/Home/PlaceOrder'
 const App = () => {
   return (
     <div className='app'>
       <Navbar></Navbar>
-      
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/cart' element={<Cart/>}></Route>
+        <Route path='/placeOrder' element={<PlaceOrder></PlaceOrder>}></Route>
+      </Routes>
     </div>
   )
 }
