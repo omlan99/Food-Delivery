9b0d61f9bd9cdc37e3efc6ac6eb04633de1edf67
Amlan <omlan10616@gmail.com>
2025-07-24 20:18:11 +0000

    added sorecontext

--- src/Context/AppContext.jsx
+++
@@ -1,9 +0,0 @@
-import { Children, createContext } from "react";
-
-const AppContext = createContext()
-export const AppContextProvider = ({children}) => {
- const value ={}
- return <AppContext.Provider value={value}>
-    {children}
- </AppContext.Provider>
-}
