fb407f256f72e389b3b9696054dc41b2a3525bd5
Amlan <omlan10616@gmail.com>
2025-07-31 17:47:01 +0000

    added link to navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import "./Navbar.css"
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom';
 const Navbar = () => {
   const [menu, setMenu] = useState("home");
 
@@ -9,10 +10,10 @@ const Navbar = () => {
     <div className='navbar'>
       <img src={assets.logo} className='logo'/>
       <ul className="navbar-menu">
-        <li className={menu==="home"?"active": ""}>Home</li>
-        <li className={menu==="menu"? "active": ""}>Menu</li>
-        <li className={menu ==="mobile-app"?"active" : ""}>Mobile App</li>
-        <li className={menu === "contact-us"?"active" : ""}>Contact Us</li>
+        <Link to="/" onClick={() => setMenu("home")} className={menu==="home"?"active": ""}>Home</Link>
+        <a href='#explore-menu' onClick={()=> setMenu("menu")} className={menu==="menu"? "active": ""}>Menu</a>
+        <a href='#app-download' onClick={()=> setMenu("mobile-app")} className={menu ==="mobile-app"?"active" : ""}>Mobile App</a>
+        <a href='#footer' onClick={()=> setMenu("contact-us")} className={menu === "contact-us"?"active" : ""}>Contact Us</a>
       </ul>
       <div className='navbar-right'>
         <img src={assets.search_icon} alt="" />
